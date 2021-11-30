270da8dd2fb64621edb0e0d809f7ab47b6cc76ef
andron111 <kli4ev2015@yandex.ru>
2021-11-30 19:41:14 +0300

    Initial commit

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -11,7 +11,18 @@ def home(request):
 
 def password(request):
     characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    length = int(request.GET.get('length', 12))
     thepassword = ''
-    for x in range(15):
+    for x in range(length):
         thepassword += random.choice(characters)
     return render(request, 'generator/password.html', {'password': thepassword})
